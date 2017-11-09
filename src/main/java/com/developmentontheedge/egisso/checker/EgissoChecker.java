251da9849419dc7459f8bbee4c835e072d00040f
dimka <dimka@developmentontheedge.com>
2017-11-09 16:44:51 +0700

    + error summary

--- src/main/java/com/developmentontheedge/egisso/checker/EgissoChecker.java
+++ src/main/java/com/developmentontheedge/egisso/checker/EgissoChecker.java
@@ -2,6 +2,9 @@ package com.developmentontheedge.egisso.checker;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
@@ -15,63 +18,78 @@ import org.xml.sax.SAXParseException;
 
 public class EgissoChecker
 {
-	public static final String XSD_LOCAL_MSZ = "10.05.I-1.0.3.xsd";
+    public static final String XSD_LOCAL_MSZ = "10.05.I-1.0.3.xsd";
 
-	protected static Validator createValidator(String schemaFileName) throws SAXException
-	{
-    	File file = new File(EgissoChecker.class.getClassLoader().getResource(schemaFileName).getFile());
-    	
+    protected static Validator createValidator(String schemaFileName) throws SAXException
+    {
+        File file = new File(EgissoChecker.class.getClassLoader().getResource(schemaFileName).getFile());
+
         SchemaFactory factory = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
         factory.setErrorHandler(new CheckerErrorHandler());
         Schema schema = factory.newSchema(file);
-        
+
         Validator validator = schema.newValidator();
         validator.setErrorHandler(new CheckerErrorHandler());
-        
-		return validator;
-	}
-	
+
+        return validator;
+    }
+
     public void checkLocalMSZ(File xml) throws SAXException, IOException
     {
-    	Validator validator = createValidator(XSD_LOCAL_MSZ);
-    	StreamSource xmlStream = new StreamSource(xml);
+        Validator validator = createValidator(XSD_LOCAL_MSZ);
+        StreamSource xmlStream = new StreamSource(xml);
 
         try
-        { 
+        {
             validator.validate(xmlStream);
         }
         catch(SAXParseException saxException)
         {
-        	System.out.println(saxException);
+            System.out.println(saxException);
         }
-    
+        ((CheckerErrorHandler)validator.getErrorHandler()).printErrorsSummary();
     }
-    
+
     public static class CheckerErrorHandler implements ErrorHandler
     {
-    	boolean isOK = true;
-    	int error;
-    	
-    	public void error(SAXParseException e) 
-		{
-    		isOK = false;
-    		error++;
-    		//String path = extractPath(xml, exception.lineNumber, exception.columnNumber);
-        	System.out.println("error: " + e);
-        	//e.printStackTrace();
-		}
+        boolean isOK = true;
+        int error;
+        Map<String, AtomicInteger> errors = new HashMap<>();
 
-    	public void fatalError(SAXParseException exception) 
-    	{
-    		isOK = false;
-    		error++;
-    		//String path = extractPath(xml, exception.lineNumber, exception.columnNumber);
-        	System.out.println("fatal error: " + exception);
-    	}
-
-    	public void warning(SAXParseException exception) 
+        public void error(SAXParseException e)
         {
-        	System.out.println("warning: " + exception);
+            isOK = false;
+            error++;
+            //String path = extractPath(xml, exception.lineNumber, exception.columnNumber);
+            System.out.println("error: " + e);
+
+            String detailMessage = e.getMessage();
+            errors.putIfAbsent(detailMessage, new AtomicInteger(0));
+            errors.get(detailMessage).incrementAndGet();
         }
+
+        public void fatalError(SAXParseException exception)
+        {
+            isOK = false;
+            error++;
+            //String path = extractPath(xml, exception.lineNumber, exception.columnNumber);
+            System.out.println("fatal error: " + exception);
+        }
+
+        public void warning(SAXParseException exception)
+        {
+            System.out.println("warning: " + exception);
+        }
+
+        public void printErrorsSummary()
+        {
+            System.out.println("Errors summary");
+            for( Map.Entry<String, AtomicInteger> entry : errors.entrySet() )
+            {
+                System.out.println( "count: " + entry.getValue() + " message: " + entry.getKey() );
+            }
+        }
+
     }
+
 }
--- src/test/java/com/developmentontheedge/egisso/checker/EgissoCheckerTest.java
+++ src/test/java/com/developmentontheedge/egisso/checker/EgissoCheckerTest.java
@@ -13,28 +13,28 @@ public class EgissoCheckerTest extends TestCase
 
     public void testXSDLocalMSZ()
     {
-    	String fileName = EgissoChecker.XSD_LOCAL_MSZ;
-    	File file = new File(EgissoChecker.class.getClassLoader().getResource(fileName).getFile());
-    	assertTrue("File is missing: " + file.getAbsolutePath(), file.exists());
+        String fileName = EgissoChecker.XSD_LOCAL_MSZ;
+        File file = new File(EgissoChecker.class.getClassLoader().getResource(fileName).getFile());
+        assertTrue("File is missing: " + file.getAbsolutePath(), file.exists());
     }
-    
+
     public void testExample()
     {
-    	// example for PFR
-    	String fileName = "10.05.I-1.0.0.sample.1.xml"; 
-    	
-    	File file = new File(relativePath + fileName);
-    	assertTrue("File is missing: " + file.getAbsolutePath(), file.exists());
+        // example for PFR
+        String fileName = "10.05.I-1.0.0.sample.1.xml"; 
+
+        File file = new File(relativePath + fileName);
+        assertTrue("File is missing: " + file.getAbsolutePath(), file.exists());
     }
 
-    public void testLocalMSZ() throws SAXException, IOException 
+    public void testLocalMSZ() throws SAXException, IOException
     {
-    	//String fileName = "10.05.I-1.0.0.sample.1.xml"; 
-    	String fileName = "local-MSZ.xml"; 
-    	File file = new File(relativePath + fileName);
+        //String fileName = "10.05.I-1.0.0.sample.1.xml";
+        String fileName = "local-MSZ.xml"; 
+        File file = new File(relativePath + fileName);
 
-    	EgissoChecker checker = new EgissoChecker();
-    	checker.checkLocalMSZ(file);
+        EgissoChecker checker = new EgissoChecker();
+        checker.checkLocalMSZ(file);
     }
-   
+
 }
